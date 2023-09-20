a74f47821763723d51a12b30e872711f47450f02
Hubert Gruniaux <hgruniaux@clipper.ens.fr>
2023-09-20 15:08:59 +0200

    Testefghgf

--- client.cpp
+++ client.cpp
@@ -1,1 +1,11 @@
+#include <sys/socket.h>
 
+#include "common.hpp"
+
+void client() {
+    int file_desc = socket(AF_INET, SOCK_STREAM, TCP_PROTOCOL);
+    if (file_desc) {}
+
+    sockaddr addr;
+    connect(file_desc, &addr, sizeof addr);
+}
