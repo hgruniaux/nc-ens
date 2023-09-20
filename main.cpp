ad1c23027c5f33635e60344158abfff8930fdc10
Hubert Gruniaux <hgruniaux@clipper.ens.fr>
2023-09-20 16:16:12 +0200

    Implement a simple driver

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
-#include <sys/socket.h>
+#include <string_view>
+#include <string>
 
 int main(int argc, char* argv[]) {
+    int port = 8888;
+    std::string_view host;
+    bool is_server = false;
+
+    for (int i = 0; i < argc; ++i) {
+        std::string_view arg = argv[i];
+        if (arg == "--port") {
+            ++i;
+            port = std::atoi(argv[i]);
+        } else if (arg == "--host") {
+            ++i;
+            host = argv[i];
+        } else if (arg == "--server") {
+            is_server = true;
+        }
+    }
+
+    if (is_server) {
+        printf("Creating server at localhost:%d\n", port);
+        if (!server()) {
+            fprintf(stderr, "Failed to create server...");
+        }
+    } else { // is_client
+        printf("Connecting to server at %s:%d\n", host.data(), port);
+        if (!client(host.data(), port)) {
+            fprintf(stderr, "Failed to create client...");
+        }
+    }
+
     return 0;
 }
